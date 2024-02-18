e9025dfd972acc811f1d92a138609297589361bb
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-02-17 21:14:24 -0500

    add finished navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './navbar.css';
+import { faGraduationCap, faCommentDots, faFileCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Navbar() {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <nav className={expanded ? "nav-expanded" : ""} onMouseEnter={() => setExpanded(true)} onMouseLeave={() => setExpanded(false)}>
+            <div className="modes">
+                <a href="#">
+                    <i className="mode">
+                        <FontAwesomeIcon icon={faCommentDots} className='icon' />  
+                        <span className="label">Chat</span>
+                    </i>
+                </a>
+                <a href="#">
+                    <i className="mode">
+                        <FontAwesomeIcon icon={faGraduationCap} className='icon' />               
+                        <span className="label">Learn</span>
+                    </i>
+                </a>
+                <a href="#">
+                    <i className="mode">
+                        <FontAwesomeIcon icon={faFileCircleQuestion} className='icon' />  
+                        <span className="label">Quiz</span>
+                    </i>
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
