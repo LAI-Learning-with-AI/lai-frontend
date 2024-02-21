ecaca88dfe74d1dbed7d5072f1bfb430ea4344d4
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-02-21 04:25:02 -0500

    add sidebar for chats mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/navbar/navbar.tsx'
-import App from './App.tsx'
+import Chat from './pages/chat-mode/chat.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar />
-    <App />
+    <Chat />
   </React.StrictMode>,
 )
---
+++ src/pages/chat-mode/chat.tsx
@@ -0,0 +1,70 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './chat.css';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
+
+
+function Chat() {
+    return (
+        <div className="sidebar">
+            <div className="search">
+                test
+            </div>
+            <div className="recent">
+                <FontAwesomeIcon icon={faCommentDots} />
+                <text className='recent-label'>RECENTS</text>
+            </div>
+            <div className="chats">
+                <div className="chat">
+                    <div className="summary">
+                        Chat 1 Summary
+                    </div>
+                    <div className="description">
+                        prompt...
+                    </div>
+                </div>
+                <div className="chat">
+                    <div className="summary">
+                        Chat 1 Summary
+                    </div>
+                    <div className="description">
+                        prompt...
+                    </div>
+                </div>
+                <div className="chat">
+                    <div className="summary">
+                        Chat 1 Summary
+                    </div>
+                    <div className="description">
+                        prompt...
+                    </div>
+                </div>
+                <div className="chat">
+                    <div className="summary">
+                        Chat 1 Summary
+                    </div>
+                    <div className="description">
+                        prompt...
+                    </div>
+                </div>
+                <div className="chat">
+                    <div className="summary">
+                        Chat 1 Summary
+                    </div>
+                    <div className="description">
+                        prompt...
+                    </div>
+                </div>
+                <div className="chat">
+                    <div className="summary">
+                        Chat 1 Summary
+                    </div>
+                    <div className="description">
+                        prompt...
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Chat;
