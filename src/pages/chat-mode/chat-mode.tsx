68f5917b05973d3d67a1af48290702e35fb29bef
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-02-23 04:13:36 -0500

    add fetch to receive chats from backend

--- src/pages/chat-mode/chat-mode.tsx
+++ src/pages/chat-mode/chat-mode.tsx
@@ -2,21 +2,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './chat-mode.css';
 import Chat from '../../components/modes/chat.tsx'
 import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
+interface ChatState {
+    chats: string[];
+    description: string;
+    title: string;
+}
 
 function ChatMode() {
+    const [chats, setChats] = useState<ChatState[]>([])
+
     useEffect(() => {
         fetch(`${import.meta.env.VITE_SERVER}/getChats`)
-            .then((response) => {
-                response.json().then ((res) => {
-                    console.log(res);
-                });
+            .then(response => response.json())
+            .then((res: ChatState[]) => {
+                setChats(res);
             })
-            .catch((error) => {
+            .catch(error => {
                 console.error(error);
-            })
-    });
+            });
+    }, []);
 
     return (
         <div className="sidebar">
@@ -28,55 +34,9 @@ function ChatMode() {
                 <text className='recent-label'>RECENTS</text>
             </div>
             <div className="chats">
-                <Chat title='Chat 1 Summary' description='Topics...' />
-                <div className="chat">
-                    <div className="summary">
-                        Chat 1 Summary
-                    </div>
-                    <div className="description">
-                        prompt...
-                    </div>
-                </div>
-                <div className="chat">
-                    <div className="summary">
-                        Chat 1 Summary
-                    </div>
-                    <div className="description">
-                        prompt...
-                    </div>
-                </div>
-                <div className="chat">
-                    <div className="summary">
-                        Chat 1 Summary
-                    </div>
-                    <div className="description">
-                        prompt...
-                    </div>
-                </div>
-                <div className="chat">
-                    <div className="summary">
-                        Chat 1 Summary
-                    </div>
-                    <div className="description">
-                        prompt...
-                    </div>
-                </div>
-                <div className="chat">
-                    <div className="summary">
-                        Chat 1 Summary
-                    </div>
-                    <div className="description">
-                        prompt...
-                    </div>
-                </div>
-                <div className="chat">
-                    <div className="summary">
-                        Chat 1 Summary
-                    </div>
-                    <div className="description">
-                        prompt...
-                    </div>
-                </div>
+                {chats.map((chat) => (
+                    <Chat title={chat.title} description={chat.description} />
+                ))}
             </div>
         </div>
     );
