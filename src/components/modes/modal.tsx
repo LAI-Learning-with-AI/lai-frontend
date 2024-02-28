7f894e6c3cfc4ab98d2da14c923c70ed8cfd5b10
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-02-27 21:20:41 -0500

    add quiz creation modal

---
+++ src/components/modes/modal.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, useEffect } from "react";
+import './modal.css';
+
+interface ModalProps {
+    open: boolean;
+    close: () => void;
+    children: ReactElement;
+}
+
+const Modal: React.FC<ModalProps> = ({ open, close, children }) => {
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            const modal = document.getElementById("modal-main");
+            if (modal && !modal.contains(event.target as Node)) {
+                close();
+            }
+        };
+
+        if (open) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [open, close]);
+
+    return (
+        <div className={`modal ${open ? "display-block" : "display-none"}`}>
+            <div id="modal-main" className="modal-main">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- src/pages/quiz-mode/quiz-mode.tsx
+++ src/pages/quiz-mode/quiz-mode.tsx
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './quiz-mode.css';
 import Quiz from '../../components/modes/quiz.tsx'
-import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots, faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react'
+import Modal from '../../components/modes/modal.tsx';
 
 interface QuizState {
     title: string;
@@ -13,6 +14,11 @@ interface QuizState {
 
 function ChatMode() {
     const [quizzes, setQuizzes] = useState<QuizState[]>([])
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    function toggleModal() {
+        setShowModal(!showModal);
+    }
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_SERVER}/getQuizzes`)
@@ -26,18 +32,76 @@ function ChatMode() {
     }, []);
 
     return (
-        <div className="quiz-sidebar">
-            <div className="quiz-search">
-                Quiz
-            </div>
-            <div className="quiz-recent">
-                <FontAwesomeIcon icon={faCommentDots} />
-                <text className='chat-recent-label'>RECENTS</text>
-            </div>
-            <div className="quizzes">
-                {quizzes.map((quiz) => (
-                    <Quiz title={quiz.title} tags={quiz.tags} />
-                ))}
+        <div>
+            <div className="quiz-sidebar">
+                <Modal open={showModal} close={toggleModal}>
+                    <div className='modal-content'>
+                        <div className='modal-heading'>
+                            <div className='modal-headers'>
+                                <div className='modal-name-h1'>
+                                    Quiz {quizzes.length+1}
+                                </div>
+                                <div className='modal-name-h2'>
+                                    Set up your quiz
+                                </div>
+                            </div>
+                            <FontAwesomeIcon className='modal-icon' icon={faClipboardCheck} />
+                        </div>
+                        <div className='modal-settings'>
+                            <div className='modal-setting'>
+                                Questions
+                                <label className="number-box">
+                                    <input type='number' defaultValue='20' min='0' max='50' />
+                                </label>
+                            </div>
+                            <div className='modal-setting'>
+                                True/False
+                                <label className="switch">
+                                    <input type="checkbox" />
+                                    <span className="slider round" />
+                                </label>
+                            </div>
+                            <div className='modal-setting'>
+                                Multiple Choice
+                                <label className="switch">
+                                    <input type="checkbox" />
+                                    <span className="slider round" />
+                                </label>
+                            </div>
+                            <div className='modal-setting'>
+                                Short Answer
+                                <label className="switch">
+                                    <input type="checkbox" />
+                                    <span className="slider round" />
+                                </label>
+                            </div>
+                            <div className='modal-setting'>
+                                Coding
+                                <label className="switch">
+                                    <input type="checkbox" />
+                                    <span className="slider round" />
+                                </label>
+                            </div>
+                        </div>
+                        <div className='modal-input'>
+                            Quiz Topics
+                            <textarea placeholder='topic1, topic2, topic3, ...' />
+                        </div>
+                    </div>
+                </Modal>
+                <div className="quiz-search">
+                    Quiz
+                    <button onClick={toggleModal}>poop</button>
+                </div>
+                <div className="quiz-recent">
+                    <FontAwesomeIcon icon={faCommentDots} />
+                    <text className='chat-recent-label'>RECENTS</text>
+                </div>
+                <div className="quizzes">
+                    {quizzes.map((quiz) => (
+                        <Quiz title={quiz.title} tags={quiz.tags} />
+                    ))}
+                </div>
             </div>
         </div>
     );
