ea498356753155d33764bb0f4317830bc9801959
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-02-28 22:17:26 -0500

    fixed sizing issues and added buttons to responses

--- src/components/modes/response.tsx
+++ src/components/modes/response.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 import './response.css'
+import { faThumbsUp, faThumbsDown, faRotateRight, faInfo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface ResponseProps {
     children: ReactNode;
@@ -7,7 +9,27 @@ interface ResponseProps {
 
 const Response: React.FC<ResponseProps> = ( { children }) => {
     return (
-        <div className='response-text'>{children}</div>
+        <div className='response'>      
+            {children}
+            <div className='response-buttons'>
+                <div className='left'>
+                    <button className='icon'>
+                        <FontAwesomeIcon icon={faThumbsUp} />
+                    </button>
+                    <button className='icon'>
+                        <FontAwesomeIcon icon={faThumbsDown} />
+                    </button>
+                    <button className='icon'>
+                        <FontAwesomeIcon icon={faRotateRight} />
+                    </button>
+                </div>
+                <div className='right'>
+                    <button className='icon'>
+                        <FontAwesomeIcon icon={faInfo} />
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
