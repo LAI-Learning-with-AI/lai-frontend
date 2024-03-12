8262c0ed2b57e3f43c5c6204d6f19b804e6a3415
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-03-11 20:12:25 -0400

    add chat waiting symbol and enable enter to submit prompts

--- src/pages/chat-mode/chat-mode.tsx
+++ src/pages/chat-mode/chat-mode.tsx
@@ -6,8 +6,10 @@ import { faCommentDots, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState, useRef  } from 'react'
 import Response from '../../components/modes/response.tsx';
 import Textarea from 'react-expanding-textarea'
+import spinner from '../../assets/chat-loading.svg'
 import { useAuth0 } from '@auth0/auth0-react';
 
+// interface for chat objects
 interface ChatState {
     chats: string[];
     description: string;
@@ -16,17 +18,21 @@ interface ChatState {
 }
 
 function ChatMode() {
+    // vars
     const { isAuthenticated, user } = useAuth0();
     const [chats, setChats] = useState<ChatState[]>([]);
+    const [waiting, setWaiting] = useState<boolean>(false);
     const [prompt, setPrompt] = useState<string>('');
     const [chat, setChat] = useState<ChatState | null>(null);
     const chatContainerRef = useRef<HTMLDivElement>(null);
 
+    // function to scroll to bottom.
     const scrollToBottom = () => {
         if (chatContainerRef.current !== null)
             chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
     };
 
+    // call backend route to create a new chat object for the user.
     const createChat = () => {
         fetch(`${import.meta.env.VITE_SERVER}/createChat`, {
             method: 'POST',
@@ -46,7 +52,12 @@ function ChatMode() {
         });
     }
 
+    /*
+        call backend route /generateResponse to retrieve response to submitted prompts.
+        each time the function is called, display loading and prevent additional submissions.
+    */
     const getResponse = (prompt: string) => {
+        setWaiting(true);
         fetch(`${import.meta.env.VITE_SERVER}/generateResponse`, {
             method: 'POST',
             headers: {
@@ -64,16 +75,29 @@ function ChatMode() {
         })
         .then(response => response.json())
         .then(res => {
+            setWaiting(false);
             chat?.chats.push(prompt)
             chat?.chats.push(res.response)
             setPrompt('');
-            scrollToBottom();
         })
         .catch(error => {
+            setWaiting(false);
             console.error(error);
         });
     }
 
+    /* 
+        function that allows users to submit prompts using the enter key in addition to the submit button, 
+        with the ability to shift + enter to enter a newline.
+    */
+    const handleEnter = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter' && !event.shiftKey && !waiting) {
+            event.preventDefault();
+            getResponse(prompt);
+        }
+    };
+
+    // call backend route /chats to retrieve chat data for a user.
     const getChats = () => {
         fetch(`${import.meta.env.VITE_SERVER}/chats`, {
             method: 'POST',
@@ -93,10 +117,12 @@ function ChatMode() {
         });
     }
 
+    // when the user object mounts, retrieve the chats for the user
     useEffect(() => {
         getChats();
     }, [user]);
 
+    // scroll to bottom of each chat the user clicks on
     useEffect(() => {
         scrollToBottom();
     }, [chat?.chats])
@@ -130,8 +156,12 @@ function ChatMode() {
                 )}
                 {chat && (                
                     <div className="chat-input">
-                        <Textarea value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Enter prompt here..." />
-                        <button onClick={() => {getResponse(prompt)}} className="chat-input-send">
+                        <img className={`img ${waiting ? "display-block" : "display-none"}`} src={spinner} alt="Loading..." />
+                        <Textarea className={`textarea ${waiting ? "display-none" : "display-block"}`} value={prompt} onChange={(e) => setPrompt(e.target.value)} onKeyDown={handleEnter} placeholder="Enter prompt here..." />
+                        <button onClick={() => {
+                            if (!waiting)
+                                getResponse(prompt);
+                        }} className={`chat-input-send ${waiting ? "disabled" : "enabled"}`}>
                             <FontAwesomeIcon icon={faArrowRight} />
                         </button>
                     </div>
