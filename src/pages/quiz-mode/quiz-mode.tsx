449fd0c5c6451868e49fd0b92fdc29be32652644
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-03-14 17:24:02 -0400

    add section for recommended quizzes

--- src/pages/quiz-mode/quiz-mode.tsx
+++ src/pages/quiz-mode/quiz-mode.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './quiz-mode.css';
 import { useAuth0 } from "@auth0/auth0-react";
 import Quiz from '../../components/modes/quiz.tsx'
-import { faCommentDots, faClipboardCheck, faGear, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots, faClipboardCheck, faGear, faRightFromBracket, faChevronUp, faChevronDown, faRefresh } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Modal from '../../components/modes/modal.tsx';
@@ -17,23 +17,31 @@ interface QuizState {
 }
 
 function QuizMode() {
-    const { isAuthenticated, user, logout } = useAuth0();
+    // vars
+    const { isAuthenticated, user, logout, loginWithRedirect } = useAuth0();
     const navigate = useNavigate();
     const [quizzes, setQuizzes] = useState<QuizState[]>([]);
     const [topics, setTopics] = useState<string>('');
     const [showModal, setShowModal] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
+    const [recommended, setRecommended] = useState<boolean>(false);
     const [number, setNumber] = useState<number>(20);
     const [selectedSettings, setSelectedSettings] = useState<string[]>([]);
+    const [randomQuizzes, setRandomQuizzes] = useState<QuizState[]>([]);
 
+    // function to handle toggle settings for the create quizzes modal
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>, setting: string) => {
         const isChecked = event.target.checked;
         if (isChecked) setSelectedSettings(prevSettings => [...prevSettings, setting]);
         else setSelectedSettings(prevSettings => prevSettings.filter(item => item !== setting));
     };
 
+    // function to make a POST request to backend to create a new quiz in the DB for user
     const createQuiz = () => {
+        // loading screen
         toggleLoading();
+
+        // request
         fetch(`${import.meta.env.VITE_SERVER}/generatequiz`, {
             method: 'POST',
             headers: {
@@ -49,22 +57,44 @@ function QuizMode() {
         })
         .then(response => response.json())
         .then((res) => {
+            // remove loading screen
             toggleLoading();
+
+            // move into quiz
             navigate(`/quiz/${res.quiz_id}`)
         })
         .catch(error => {
+            // remove loading screen
+            toggleLoading();
             console.error(error);
         });
     }
 
+    // function to toggle modal state
     function toggleModal() {
         setShowModal(!showModal);
     }
 
+    // function to toggle loading state
     const toggleLoading = () => {
         setLoading(!loading);
     }
 
+    // function to toggle recommended quizzes state
+    const toggleRecommended = () => {
+        setRecommended(!recommended);
+    }
+
+    // function to randomize recommended quizzes
+    const recRandomizer = () => {
+        // shuffle
+        const shuffled = [...quizzes];
+        shuffled.sort(() => Math.random() - 0.5);
+        // Take the first three quizzes
+        setRandomQuizzes(shuffled.slice(0, 3));
+    };
+
+    // when user object mounts, fetch quizzes from the backend
     useEffect(() => {
         fetch(`${import.meta.env.VITE_SERVER}/quizzes`, {
             method: 'POST',
@@ -83,10 +113,6 @@ function QuizMode() {
             console.error(error);
         });
     }, [user]);
-
-    if (!user || !isAuthenticated) {
-        return null;
-    }
 
     return (
         <div>
@@ -164,14 +190,39 @@ function QuizMode() {
                         </div>
                     </div>
                 </div>
-                <div className="quiz-recent">
-                    <FontAwesomeIcon icon={faCommentDots} />
-                    <text className='chat-recent-label'>RECENTS</text>
+                <div className='quiz-recommend-container'>
+                    <div className="quiz-label-container">
+                        <div className="quiz-label">
+                            <FontAwesomeIcon icon={faCommentDots} />
+                            <span className='quiz-label'>RECOMMENDED</span>
+                        </div>
+                        <div className="quiz-container-buttons">
+                            <button className="randomize" onClick={() => {recRandomizer(); console.log(randomQuizzes)} }>
+                                <FontAwesomeIcon className='minimize-icon' icon={faRefresh} />
+                            </button>
+                            <button className="minimize" onClick={() => toggleRecommended() }>
+                                <FontAwesomeIcon className='minimize-icon' icon={recommended ? faChevronUp : faChevronDown} />
+                            </button>
+                        </div>
+                    </div>
+                    <div className={`recommended-quizzes ${recommended ? "enable" : "disable"}`}>
+                        {randomQuizzes.map((quiz) => (
+                            <Quiz name={quiz.topics} topics={"Recommended by 1 teacher and 5 students"} onClick={() => navigate(`/quiz/${quiz.id}`)} />
+                        ))}
+                    </div>
                 </div>
-                <div className="quizzes">
-                    {quizzes.map((quiz) => (
-                        <Quiz name={quiz.name} topics={quiz.topics} onClick={() => navigate(`/quiz/${quiz.id}`)} />
-                    ))}
+                <div className='quiz-container'>
+                    <div className="quiz-label-container">
+                        <div className="quiz-label">
+                            <FontAwesomeIcon icon={faCommentDots} />
+                            <span className='quiz-label'>RECENTS</span>
+                        </div>
+                    </div>
+                    <div className="quizzes">
+                        {quizzes.map((quiz) => (
+                            <Quiz name={quiz.name} topics={quiz.topics} onClick={() => navigate(`/quiz/${quiz.id}`)} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
