487eba40c780be52347cc04ecc2ac3d1e5f08179
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-03-15 17:22:58 -0400

    add signup and login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,18 @@ import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/signup/signup.tsx'
 import Profile from './pages/profile.tsx'
 import './App.css'
+import Login from './pages/signup/login.tsx'
 
 
 function App() {
   return (
       <Router>
-        {enableNavbar() && <Navbar />}
+        <Navbar />
         <div className='app-container'>
           <div className='navbar-spacer'></div>
           <Routes>
             <Route path='/signup' element={<SignUp />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/' element={<ChatMode />} />
             <Route path='/quizzes' element={<QuizMode />} />
@@ -24,11 +26,6 @@ function App() {
         </div>
       </Router>
   )
-}
-
-function enableNavbar() {
-  const excludedPaths = ['/signup'];
-  return !excludedPaths.includes(window.location.pathname);
 }
 
 export default App
--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,29 +1,57 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from 'react-router-dom';
 import './signup.css';
+import { useState } from "react";
 
 function SignUp() {
     const navigate = useNavigate();
+    const [first, setFirst] = useState<string>('');
+    const [last, setLast] = useState<string>('');
+    const [studentId, setStudentId] = useState<string>('');
+
 
     // drop in Auth0 authentication service
-    const { isAuthenticated, user, loginWithRedirect } = useAuth0();
+    const { isAuthenticated, user, loginWithPopup } = useAuth0();
 
-    // if user is already logged in, redirect to /
-    if (isAuthenticated && user)
-        navigate('/')
+    // when user logs in create user in db
+    if (isAuthenticated && user) {
+        fetch(`${import.meta.env.VITE_SERVER}/createUser`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "userId": user?.sub,
+                "email": user?.email,
+                "name": first + ' ' + last
+            })
+        })
+        .then(() => navigate('/'))
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
     return (
         // signup button which redirects to Auth0 for signup and login
         <div className="signup-container">
+            <div className='signup-image'></div>
             <div className="signup-main">
-                <div className="signup-header">Signup</div>
+                <div className="signup-header">
+                    <div className="redirect-container">
+                        <button className="signup-redirect enabled">Signup</button>
+                        <button className="signup-redirect disabled" onClick={() => navigate('/login')}>Login</button>
+                    </div>
+                </div>
                 <div className="signup-middle">
-                    <input placeholder="Full Name"></input>
-                    <input placeholder="Student ID"></input>
+                    <div className="two-column">
+                        <input placeholder="First Name" onChange={(e) => setFirst(e.target.value)} />
+                        <input placeholder="Last Name" onChange={(e) => setLast(e.target.value)} / >
+                    </div>
+                    <input className='student-input' placeholder="Student ID" onChange={(e) => setStudentId(e.target.value)}></input>
                 </div>
                 <div className="signup-footer">
-                    <button className="signup-button" onClick={() => loginWithRedirect().then(() => {
-                        console.log(user);
-                    })}>Continue with Auth0</button>
+                    <button className="signup-button" onClick={() => loginWithPopup()}>Continue with Auth0</button>
                 </div>
             </div>
         </div>
