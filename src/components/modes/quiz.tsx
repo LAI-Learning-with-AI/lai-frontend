5dbce579defd02986585abc1ac338b8c55b4fb76
Nicholas Dinh <80604823+nick-dinh@users.noreply.github.com>
2024-04-05 00:59:37 -0400

    add progress circle + retry button

--- src/components/modes/quiz.tsx
+++ src/components/modes/quiz.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './quiz.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
 
 interface Props {
     name: string;
@@ -10,11 +14,32 @@ interface Props {
 const Quiz: React.FC<Props> = ({ name, topics, onClick } ) => {
     return (
         <div className='quiz' onClick={onClick}>
-            <div className='quiz-title'>
-                {name}
+            <div className='quiz-descriptions'>
+                <div className='quiz-title'>
+                    {name}
+                </div>
+                <div className='quiz-tags'>
+                    {topics}
+                </div>
             </div>
-            <div className='quiz-tags'>
-                {topics}
+            <div className='quiz-buttons'>
+                <div className='icons'>
+                    <button className='icon'>
+                        <FontAwesomeIcon icon={faRotateRight} />
+                    </button>
+                    <div className='icon progress'>
+                        <CircularProgressbar
+                            value={80}
+                            text={`80%`}
+                            styles={buildStyles({
+                                textColor: "#A6FF86",
+                                pathColor: "#A6FF86",
+                                trailColor: "#FF4E2E",
+                                textSize: "30px"
+                            })}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
